e4b8d0adc3dfbef278990a47743e49bd799acf9a
Clint Ast <clintast@gmail.com>
2018-08-01 16:52:42 -0500

    added companies

--- schema/schema.js
+++ schema/schema.js
@@ -5,8 +5,25 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then( res => res.data);
+      }
+    }
+  })
+});
 
 // const users = [
 //   { id: '1', firstName: 'Eve', age: 4 },
@@ -15,11 +32,18 @@ const {
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString},
     firstName: { type: GraphQLString},
-    age: { type: GraphQLInt}
-  }
+    age: { type: GraphQLInt},
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -34,6 +58,14 @@ const RootQuery = new GraphQLObjectType({
 
         // return _.find(users, { id: args.id });
         // the commented out code throughout scehma would be the way to query static data.
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
