5dabe0352d018447fca647dfb1d69a85bdac5b7a
Clint Ast <clintast@gmail.com>
2018-08-01 15:39:35 -0500

    initial commit

---
+++ schema/schema.js
@@ -0,0 +1,44 @@
+const graphql = require('graphql');
+// const _ = require('lodash');
+const axios = require('axios');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+// const users = [
+//   { id: '1', firstName: 'Eve', age: 4 },
+//   { id: '2', firstName: 'Ellie', age: 3}
+// ];
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString},
+    firstName: { type: GraphQLString},
+    age: { type: GraphQLInt}
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
+
+        // return _.find(users, { id: args.id });
+        // the commented out code throughout scehma would be the way to query static data.
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
